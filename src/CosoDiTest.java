f7e83ecee5c2adfacb679ae96bad17972fdde7ef
Team Antani <antani@trovaprezzi.it>
2019-07-08 14:38:28 +0200

    receipt with one good

--- src/CosoDiTest.java
+++ src/CosoDiTest.java
@@ -2,7 +2,18 @@ import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
-public class CosoDiTest {
+public class CosoDiTest { // TODO: rename in ReceiptTest
+
+
+    @Test
+    public void one_row_for_one_good() {
+        Receipt receipt = new Receipt();
+        receipt.add(new Good());
+
+        String result = receipt.calculate();
+
+        assertEquals("good name", result);
+    }
 
     @Test
     public void empty_result_if_no_goods_added() {
--- src/Receipt.java
+++ src/Receipt.java
@@ -1,5 +1,16 @@
 public class Receipt {
+    private Good good;
+
     public String calculate() {
+
+        if(null != good)
+            return good.name();
+
         return "";
+    }
+
+    public void add(Good good) {
+
+        this.good = good;
     }
 }
