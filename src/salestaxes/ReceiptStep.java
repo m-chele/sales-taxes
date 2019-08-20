51ce4a20a61a8d934e4274665f95102229e84b42
Team Antani <antani@trovaprezzi.it>
2019-08-20 14:21:10 +0200

    refactor: minor refactor

--- src/salestaxes/ReceiptStep.java
+++ src/salestaxes/ReceiptStep.java
@@ -1,9 +1,10 @@
 package salestaxes;
 
+import salestaxes.ReceiptBuilder.Receipt;
 import salestaxes.goods.Good;
 
 public interface ReceiptStep {
     ReceiptStep add(Good good);
 
-    ReceiptBuilder.Receipt build();
+    Receipt build();
 }
